4bcf19d4b20ee3ae8622a06c62919d712ab314a6
kikilagou <kiki-1@hotmail.co.uk>
2019-11-21 23:02:20 +0000

    creating get method for retrieving book information from API

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Here we go...
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import axios from 'axios'
 
 class App extends React.Component {
+
+  async postBooks() {
+  }
+
+  async getBooks() {
+    await axios
+      // This is where the data is hosted
+      .get("https://www.goodreads.com/search.xml?key=Q4uAl7lxzaYA85u59Kzvg&q=Ender%27s+Game")
+      .then(response => {
+        console.log(response.data)
+      }
+      )
+      .catch();
+  }
+
   render() {
     return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Here we go...
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.getBooks}>click me</button>
+
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
               </a>
-            </header>
-          </div>
-        );
+        </header>
+      </div>
+    );
   }
 }
 
