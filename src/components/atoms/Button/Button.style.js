3eff6f92c07b12de0c70e7737eb7a745e28be804
kikilagou <kiki-1@hotmail.co.uk>
2019-12-23 23:50:03 +0000

    adding some media queries to atomic components

--- src/components/atoms/Button/Button.style.js
+++ src/components/atoms/Button/Button.style.js
@@ -71,6 +71,19 @@ const ButtonWrapper = styled.button`
   &.rounded {
     border-radius: 3px;
   }
+
+  // Media specific sizing
+  @media screen and (min-width: ${theme.values.mobile1}) {
+    font: ${theme.values.smallFont};
+  }
+
+  @media screen and (min-width: ${theme.values.desktop}) {
+    font: ${theme.values.defaultFont};
+  }
+
+  @media screen and (min-width: ${theme.values.highres}) {
+    font: ${theme.values.largeFont};
+  }
 `;
 
 export { ButtonWrapper };
