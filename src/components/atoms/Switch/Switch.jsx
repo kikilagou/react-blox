f389fc85b3e029b73e76fec4b36fedbe32a037f4
kikilagou <kiki-1@hotmail.co.uk>
2020-01-28 23:35:30 +0000

    created template for switch atom

---
+++ src/components/atoms/Switch/Switch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SwitchWrapper } from "./Switch.style";
+
+// option - build it label with input and span
+export default () => {
+  return (
+    <SwitchWrapper>
+      
+    </SwitchWrapper>
+  );
+};
